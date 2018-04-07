16c3af1f952e088363b68b68e78c895d8026a04b
Sameea Mughnee <samiyah.mughnee@gmail.com>
2018-04-07 11:34:26 -0400

    Sameea2

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/connection.js")[env];
+var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 var expressValidator = require('express-validator');
 
