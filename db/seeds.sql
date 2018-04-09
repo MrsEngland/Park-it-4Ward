9cd6af7b74d99066c289d7ec1f7957c8b850f3cb
Donna England <mrsengland@aol.com>
2018-04-09 19:38:17 -0400

    donna3

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,20 @@
 
-INSERT INTO parkinglots (lot_name, lot_address, lot_city, lot_state, lot_zip, available_spaces, 
+INSERT INTO parkinglots (lot_name, lot_address, lot_city, lot_state, lot_zip, lot_lat, lot_lng, available_spaces, 
 	                     default_time, spot_id)
-VALUES ("Parking123", "123 Main Street", "Charlotte", "NC", "28202", 10, 12, 1);
+VALUES ("ParkingA", "309 East 8th Street", "Charlotte", "NC", "28204", 35.227927, -80.836009, 10, 12, 1);
 
-INSERT INTO parkinglots (lot_name, lot_address, lot_city, lot_state, lot_zip, available_spaces, 
+INSERT INTO parkinglots (lot_name, lot_address, lot_city, lot_state, lot_zip, lot_lat, lot_lng, available_spaces, 
 	                     default_time, spot_id)
-VALUES ("Parking123", "123 Main Street", "Charlotte", "NC", "28202", 10, 12, 2);
+VALUES ("ParkingB", "325 East 9th Street", "Charlotte", "NC", "28204", 35.228532, -80.834729,  10, 12, 2);
 
-INSERT INTO parkinglots (lot_name, lot_address, lot_city, lot_state, lot_zip, available_spaces, 
+INSERT INTO parkinglots (lot_name, lot_address, lot_city, lot_state, lot_zip, lot_lat, lot_lng, available_spaces, 
 	                     default_time, spot_id)
-VALUES ("Parking123", "123 Main Street", "Charlotte", "NC", "28202", 10, 12, 3);
+VALUES ("ParkingC", "315 East 7th Street", "Charlotte", "NC", "28202", 35.227202, -80.837042, 10, 12, 3);
 
-INSERT INTO parkinglots (lot_name, lot_address, lot_city, lot_state, lot_zip, available_spaces, 
+INSERT INTO parkinglots (lot_name, lot_address, lot_city, lot_state, lot_zip, lot_lat, lot_lng, available_spaces, 
 	                     default_time, spot_id)
-VALUES ("Parkhere", "123 Anywhere Street", "Charlotte", "NC", "28202", 15, 8, 4);
+VALUES ("ParkingD", "418 East 9th Street", "Charlotte", "NC", "28202", 35.227852, -80.834025, 15, 8, 4);
 
-INSERT INTO parkinglots (lot_name, lot_address, lot_city, lot_state, lot_zip, available_spaces, 
-	                     default_time, spot_id)
-VALUES ("Parkhere", "123 Anywhere Street", "Charlotte", "NC", "28202", 15, 8, 5);
 
 
 INSERT INTO parkingspaces (spot_id)
