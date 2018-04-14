c69a778598a26ee1fb036c50c307a3d88b23e9a0
Bess Keefe <elizabeth.keefe@clt-m002.attlocal.net>
2018-04-13 20:32:18 -0400

    back to normal

--- models/auth.js
+++ models/auth.js
@@ -1,44 +1,44 @@
 
-var authController = require('../controllers/authcontroller.js');
-var express = require("express");
-var app = module.exports = express(); 
-//some app/middleware setup, etc, including 
+// var authController = require('../controllers/authcontroller.js');
+// var express = require("express");
+// var app = module.exports = express(); 
+// //some app/middleware setup, etc, including 
 
 
 
 
 
 
-module.exports = function(app, passport) {
+// module.exports = function(app, passport) {
 
-app.get('/register', authController.register);
-
-
-app.get('/login', authController.login);
+// app.get('/register', authController.register);
 
 
-app.post('/register', passport.authenticate('local-signup',  { successRedirect: '/dashboard',
-                                                    failureRedirect: '/signup'}
-                                                    ));
+// app.get('/login', authController.login);
 
 
-app.get('/dashboard',isLoggedIn, authController.dashboard);
+// app.post('/register', passport.authenticate('local-signup',  { successRedirect: '/dashboard',
+//                                                     failureRedirect: '/signup'}
+//                                                     ));
 
 
-app.get('/logout',authController.logout);
+// app.get('/dashboard',isLoggedIn, authController.dashboard);
 
 
-app.post('/login', passport.authenticate('local-signin',  { successRedirect: '/dashboard',
-                                                    failureRedirect: '/#aboutModal'}
-                                                    ));
+// app.get('/logout',authController.logout);
 
 
-function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated())
-        return next();
-
-    res.redirect('/register');
-}
+// app.post('/login', passport.authenticate('local-signin',  { successRedirect: '/dashboard',
+//                                                     failureRedirect: '/#aboutModal'}
+//                                                     ));
 
 
-}
+// function isLoggedIn(req, res, next) {
+//     if (req.isAuthenticated())
+//         return next();
+
+//     res.redirect('/register');
+// }
+
+
+// }
